274f703fdc75e042bf97c405f034a4b9c2eb1548
Your Name <you@example.com>
2024-04-23 16:03:04 +0700

    fixed avatar

--- components/Description/description.jsx
+++ components/Description/description.jsx
@@ -46,7 +46,7 @@ export default function Description() {
 
 
           <div data-scroll data-scroll-speed={0.1}>
-            <Rounded className={styles.button}>
+            <div className={styles.button}>
               <Image
                 src="/images/projects/avatar.jpg"
                 fill={true}
@@ -54,7 +54,7 @@ export default function Description() {
                 alt="avatar"
               />
 
-            </Rounded>
+            </div>
           </div>
         </div>
 
